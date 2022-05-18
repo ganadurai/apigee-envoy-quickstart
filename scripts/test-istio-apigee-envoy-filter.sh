b4e534ab936b7d8f0d2fb052ac9186d757de4d8b
Ganapathy Duraiswamy <ganadurai@google.com>
2022-05-18 00:22:48 -0500

    fixes

--- scripts/test-istio-apigee-envoy-filter.sh
+++ scripts/test-istio-apigee-envoy-filter.sh
@@ -6,7 +6,7 @@ echo "Extract the consumer key"
 
 export CONSUMER_KEY=$(curl -H "Authorization: ${TOKEN_TYPE} ${TOKEN}"  \
     -H "Content-Type:application/json" \
-    ""${MGMT_HOST}/v1/organizations/${ORG}/developers/test-envoy@google.com/apps/envoy-adapter-app-2" | \
+    "${MGMT_HOST}/v1/organizations/${ORG}/developers/test-envoy@google.com/apps/envoy-adapter-app-2" | \
     jq '.credentials[0].consumerKey'); \
     export CONSUMER_KEY=$(echo $CONSUMER_KEY|cut -d '"' -f 2); \
     echo "" && echo ""
