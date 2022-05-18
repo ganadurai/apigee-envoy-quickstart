40dd3823172cc49850d1699717082a8473d06418
Ganapathy Duraiswamy <ganadurai@google.com>
2022-05-17 21:44:37 -0500

    fixes

--- standalone-apigee-envoy-install.sh
+++ standalone-apigee-envoy-install.sh
@@ -2,25 +2,19 @@
 
 set -e
 
-echo "Installing apigee envoy PoC setup"
+echo "Installing apigee standalone envoy PoC setup"
 
-printf "\n\nstep 1 : validate.sh\n" 
-./scripts/validate.sh
-
-printf "\n\nstep 2 : service-accounts.sh\n" 
-./scripts/service-accounts.sh
-
-printf "\n\nstep 4 : download-libraries.sh\n"
+printf "\n\nstep 1 : download-libraries.sh\n"
 ./scripts/download-libraries.sh
 
-printf "\n\nstep 5 : setup-libraries.sh\n"
+printf "\n\nstep 2 : setup-libraries.sh\n"
 ./scripts/setup-libraries.sh
 
-printf "\n\nstep 6 : setup-apigee.sh\n"
+printf "\n\nstep 3 : setup-apigee.sh\n"
 ./scripts/setup-apigee.sh
 
-printf "\n\nstep 7 : setup-standalone-envoy.sh\n"
+printf "\n\nstep 4 : setup-standalone-envoy.sh\n"
 ./scripts/setup-standalone-envoy.sh
 
-printf "\n\nstep 8 : test-standalone-apigee-envoy-filter.sh\n"
+printf "\n\nstep 5 : test-standalone-apigee-envoy-filter.sh\n"
 ./scripts/test-standalone-apigee-envoy-filter.sh
