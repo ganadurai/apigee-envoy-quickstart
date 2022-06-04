23543b9ec6ab0e58714c0fd4dd89b9f716290add
Ganapathy Duraiswamy <ganadurai@google.com>
2022-06-04 01:36:39 -0500

    fixes

--- scripts/setup-libraries.sh
+++ scripts/setup-libraries.sh
@@ -33,19 +33,21 @@ then
     --password $APIGEE_PASS \
     --opdk --verbose > $CLI_HOME/config.yaml
 else
-#    $CLI_HOME/apigee-remote-service-cli provision \
-#    --organization $APIGEE_ORG \
-#    --environment $APIGEE_ENV \
-#    --runtime $APIGEE_X_HOSTNAME \
-#    --namespace $NAMESPACE \
-#    --analytics-sa $AX_SERVICE_ACCOUNT \
-#    --token $TOKEN > $CLI_HOME/config.yaml
-
-    echo $INSTALL_SCRIPTS_DIR
-    envsubst < $INSTALL_SCRIPTS_DIR/templates/config.tmpl > ${ENVOY_HOME}/config-init.yaml
-
     $CLI_HOME/apigee-remote-service-cli provision \
-    --config=${ENVOY_HOME}/config-init.yaml > $CLI_HOME/config.yaml
+    --organization $APIGEE_ORG \
+    --environment $APIGEE_ENV \
+    --runtime $APIGEE_X_HOSTNAME \
+    --namespace $NAMESPACE \
+    --analytics-sa $AX_SERVICE_ACCOUNT \
+    --token $TOKEN > $CLI_HOME/config.yaml
+
+sed -i "s/      collection_interval: 10s/      collection_interval: 10s\n    products:\n      refresh_rate: 2s/g" $CLI_HOME/config.yaml
+
+#    echo $INSTALL_SCRIPTS_DIR
+#    envsubst < $INSTALL_SCRIPTS_DIR/templates/config.tmpl > ${ENVOY_HOME}/config-init.yaml
+
+#    $CLI_HOME/apigee-remote-service-cli provision \
+#    --config=${ENVOY_HOME}/config-init.yaml > $CLI_HOME/config.yaml
 fi
 
 curl -i -v $APIGEE_X_HOSTNAME/remote-token/certs | grep 200 2>&1 >/dev/null
