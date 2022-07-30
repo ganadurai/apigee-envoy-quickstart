c0cb6c12e720f8841e14af4fd2fb80175db74b3b
Ganapathy Duraiswamy <ganadurai@google.com>
2022-07-29 19:23:01 -0500

    fixes

--- pipeline.sh
+++ pipeline.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export ENVOY_HOME=/tmp
+export ENVOY_HOME=/home
 
 export PIPELINE_TEST="true"
 
